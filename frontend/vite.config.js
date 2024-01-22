5bfa713691414d0a873471be2010dbd50d4f71ab
Nutan Kumari <31533479+nutankumari211@users.noreply.github.com>
2024-01-22 06:31:34 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
-export default {
-plugins: [react()],
-base: process.env.NODE_ENV === 'production' ? './' : '/',
-};
+export default defineConfig({
+  plugins: [react()],
+})
